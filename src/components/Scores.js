783523c51dc0cc09103c3f3db618e4309a1fe5ff
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-02-04 08:50:27 -0600

    Added a couple of components to be worked on today.

---
+++ src/components/Scores.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Scores = (props) => {
+  return(
+    <div>
+      <h2>Your Childs Score (Each child with their score can be listed here)</h2>
+    </div>
+  )
+}
+
+export default Scores
