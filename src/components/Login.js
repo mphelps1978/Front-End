94f98629939b8fcb4642e5566fda63d91efc086b
Michael Phelps <56881040+mphelps1978@users.noreply.github.com>
2020-02-07 05:00:50 -0600

    Update Login.js
    Link import

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Typography from "@material-ui/core/Typography";
+import {Link} from 'react-router-dom';
 
 class Login extends React.Component {
   state = {
